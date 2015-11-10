51731a873cd436f4c6ecdee6471f18098cc391a0
bab178 <bab178@txstate.edu>
2015-11-09 18:24:18 -0600

    Fix merging issues

--- Citron/views.py
+++ Citron/views.py
@@ -5,7 +5,6 @@ class HomePageView(TemplateView):
     template_name = "base.html"
 
     def get_context_data(self, **kwargs):
-<<<<<<< HEAD
         return super(HomePageView, self).get_context_data(**kwargs)
 
 class Aggregated_Map(TemplateView):
@@ -22,6 +21,3 @@ class Sentiment_Data(TemplateView):
 
     def get_context_data(self, **kwargs):
         return super(Sentiment_Data, self).get_context_data(**kwargs)
-=======
-        return super(HomePageView, self).get_context_data(**kwargs)
->>>>>>> 3bb92ccd4a810cc9967a5fafff18c2033c8e29d5
