718d1e41acc1a4530d164c986f314e033a2dff69
Blake Bordovsky <bab178@txstate.edu>
2015-11-12 15:34:01 -0600

    Markers and heatpoints are now associated

--- static/Aggregated_Map.js
+++ static/Aggregated_Map.js
@@ -1,43 +1,51 @@
-var map, heatmap, cityCenter = {lat: 30.317, lng: -97.743};
+var map, heatmap, cityCenter = {lat: 30.317, lng: -97.743}, postNum = 1, MAX_POINTS = 250;
 
-//Call getPoints to generate random points for heatmap
-var heatPoints = getPoints();
+// Call getPoints to generate random points for heatmap
+var heatPoints = getPoints(MAX_POINTS);
 
-var numInfoMarkers = 0;
+var InfoMarkers = [], posts = [];
 
-var InfoMarkers = [];
-
-
-posts = [];
-postNum = 1;
-
-//Initialize the map
+// Initialize the map
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 10,
 		center: cityCenter
 	});
 	
-	//Generate Heatmap from getPoints()
+	// Generate Heatmap from getPoints()
 	heatmap = new google.maps.visualization.HeatmapLayer({
 		data: heatPoints,
+		radius:5,
 		map: map
 	});
 	
-	//Add post associated to every heat point
+	// Add post associated to every heat point
 	$.ajax({
 		url:"/admin/postsJsonObject/",
 		context: document.body
 	}).done(function(data) {
-		posts = data.split("\",");
-		for(var i = 0; i < heatPoints.length; i++)
-		{
-			addMarker(heatPoints[numInfoMarkers], posts[postNum].substr(1,posts[postNum].length));
+		data = data.split('statement');
+		
+		for(var i = 0; i < data.length; i++) {
+			data[i] = data[i].substr(3,data[i].length);
 		}
-		postNum+= 2;
+		
+		// Clean garbage from posts 
+		// (post is from model statement to model poster)
+		for(var i = 0; i < data.length; i++) {
+			data[i] = data[i].split('poster')[0];
+			data[i] = data[i].substr(0,data[i].length-4);
+			posts.push(data[i]);
+		}
+		
+		for(var i = 0; i < heatPoints.length; i++) {
+			addMarker(heatPoints[InfoMarkers.length], posts[postNum].substr(1,posts[postNum].length));
+			postNum+= 1;
+		}
+		
 	});
 
-	//Add a marker to the map when called
+	// Add a marker to the map when called
 	function addMarker(location, post) {
 		
 		if(post === []) {
@@ -45,113 +53,116 @@ function initMap() {
 			return;
 		}
 		
-		//Marker
+		// Markers
 		var marker = new google.maps.Marker({
 			position: location,
 			map: map,
-			title: 'Hover Title'
+			title: location.toString()
 		});
+		
+		if(InfoMarkers.length < 20){
+			marker.setMap(map);
+		}
+		else {
+			marker.setMap(null);
+		}
 
-		//replace newline char with space
-		//post = post.replace(/\n/g, " ");
+		// replace newline char with space
+		// post = post.replace(/\n/g, " ");
 		
 		var tags = [];
 		var newTags = post.split('#');
 		
+		// Search for hashtags, split by tag and "\"
 		for(var i = 1; i < newTags.length; i++) {
-			tags.push(newTags[i].split(" ")[0]+" ");
+			tags.push(newTags[i].split(" ")[0].split("\\")[0]+" ");
 		}
 		
 		if(tags.length === 0) {
 			tags = "None";
 		}
 		
-		//InfoWindow
+		// InfoWindow
 		var contentString = 
 		'<h2>Location: '+location+'</h2>'+
 		'<post>'+post+'</post><br>'+
 		'<strong><tag>Tags: '+tags+'</tag></strong>';
 		var infowindow = new google.maps.InfoWindow({
-            content: contentString
-        });
+      content: contentString
+    });
 		
-		//Custom InfoMarker object
+		// Custom InfoMarker object
 		var InfoMarker = {
 			Marker:marker,
 			InfoWindow:infowindow,
-			heatPoint:heatPoints[numInfoMarkers],
-			index:numInfoMarkers
+			heatPoint:heatPoints[InfoMarkers.length]
 		};
 			
 		InfoMarkers.push(InfoMarker);
 		
-		//update numInfoMarkers
-		numInfoMarkers += 1;
+		// Open selected marker's InfoWindow
+		google.maps.event.addListener(marker,'click', (
+			function(marker,contentString,infowindow){ 
+				return function() {
+				   infowindow.setContent(contentString);
+				   infowindow.open(map,marker);
+				};
+			})(marker,contentString,infowindow)
+		); 
 		
-		//Open selected marker's InfoWindow
-		google.maps.event.addListener(InfoMarkers[numInfoMarkers-1].Marker, 'click', function() {
-			var current = InfoMarkers[numInfoMarkers-1].index;
-			InfoMarkers[current].InfoWindow.open(map,InfoMarkers[current].Marker);
-        });
-		
-		//Double click to delete marker
-		google.maps.event.addListener(InfoMarkers[numInfoMarkers-1].Marker, 'dblclick', function() {
-			InfoMarkers[numInfoMarkers-1].Marker.setMap(null);
-        });
 	}
 	
-	//Calls addMarker when map is clicked
+	// Called when map is clicked
 	// google.maps.event.addListener(map, 'click', function(event) {
 	// });
 	
-	// Sets the map on all InfoMarkers in the array.
+	// Sets the given map on all InfoMarkers in the array.
 	function setMapOnAll(map, array) {
 		for (var i = 0; i < array.length; i++) {
-			InfoMarkers[i].Marker.setMap(map);
+			array[i].Marker.setMap(map);
 		}
 	}
 	
 	// Removes the markers from the map, but keeps them in the array.
 	document.getElementById('hideMarkers').onclick = function hideMarkers() {
-	  setMapOnAll(null, InfoMarkers);
+	 setMapOnAll(null, InfoMarkers);
 	}
 
 	// Shows any markers currently in the array.
 	document.getElementById('showMarkers').onclick = function showMarkers() {
-	  setMapOnAll(map, InfoMarkers);
+	 setMapOnAll(map, InfoMarkers);
 	}
 
 	// Deletes all markers in the array by removing references to them.
 	document.getElementById('deleteMarkers').onclick = function deleteMarkers() {
-	  setMapOnAll(null, InfoMarkers);
-	  InfoMarkers = [];
+	 setMapOnAll(null, InfoMarkers);
+	 InfoMarkers = [];
 	}
 	
 	document.getElementById('searchTag').onclick = function searchTag() {
-	  var tag = document.getElementById('textarea').value;
-	  // infoWindow and Markers are unrelated and therefore cannot be associated together as one object, solution requires database calls and results through a query
-	  var tagged = [];
-	  var checkTag;
-	  for (var i = 0; i < InfoMarkers.length; i++) {
-		  setMapOnAll(null, InfoMarkers);
-	  }
-	  for (var i = 0; i < InfoMarkers.length; i++) {
-		  //get tag from InfoWindow
-		  checkTag = InfoMarkers[i].InfoWindow.content.split("<tag>Tag: ")[1].split("</tag>")[0]+" ";
-		  
+	 var tag = document.getElementById('textarea').value;
+	 var tagged = [];
+	 var checkTag;
+	 for (var i = 0; i < InfoMarkers.length; i++) {
+		 setMapOnAll(null, InfoMarkers);
+	 }
+	 for (var i = 0; i < InfoMarkers.length; i++) {
+		 // get tag from InfoWindow
+		 checkTag = InfoMarkers[i].InfoWindow.content.split("<tag>Tag: ")[1].split("</tag>")[0]+" ";
+		 
 		if(checkTag === tag) {
 			tagged.push(InfoMarkers[i]);
 		}
-	  }
+	 }
 
-	  for (var i = 0; i < tagged.length; i++) {
-		  setMapOnAll(map, tagged);
-	  }
+	 for (var i = 0; i < tagged.length; i++) {
+		 setMapOnAll(map, tagged);
+	 }
 
 	}
 	document.getElementById('highlight').onclick = function highlight(){
 		var cityHightlight;
-		//Parse HTML into Google Maps LatLng Objects
+		// Parse HTML into Google Maps LatLng Objects
 		$.ajax({
 			url:"/static/AustinPolygon.html",
 			context: document.body 
@@ -165,7 +176,7 @@ function initMap() {
 				paths.push(new google.maps.LatLng(x[1],x[0]));
 			}
 		
-			//DRAW THE POLYGON OR POLYLINE
+			// DRAW THE POLYGON OR POLYLINE
 			cityHightlight = new google.maps.Polygon({
 				clickable: false,
 				paths: paths,
@@ -183,20 +194,22 @@ function initMap() {
 }
 
 
-function getPoints() {
+function getPoints(MAX_POINTS) {
 	
 	var genPoints = [];
-	//Generate random points
-	for(var i = 0; i < 10; i++) {
-		genPoints.push(new google.maps.LatLng(cityCenter.lat + Math.random() % 0.1, cityCenter.lng + Math.random() % 0.1));
-		genPoints.push(new google.maps.LatLng(cityCenter.lat - Math.random() % 0.1, cityCenter.lng - Math.random() % 0.1));
+	// Generate random points
+	for(var i = 0; i < MAX_POINTS; i++) {
+		if(i < MAX_POINTS/2)
+			genPoints.push(new google.maps.LatLng(cityCenter.lat + Math.random() % 0.1, cityCenter.lng + Math.random() % 0.1));
+		else
+			genPoints.push(new google.maps.LatLng(cityCenter.lat - Math.random() % 0.1, cityCenter.lng - Math.random() % 0.1));
 	}
 	
 	return genPoints;
 }
 	
 function changeGradient() {
-  var gradient = [
+ var gradient = [
 	'rgba(0, 255, 255, 0)',
 	'rgba(0, 255, 255, 1)',
 	'rgba(0, 191, 255, 1)',
@@ -211,8 +224,8 @@ function changeGradient() {
 	'rgba(127, 0, 63, 1)',
 	'rgba(191, 0, 31, 1)',
 	'rgba(255, 0, 0, 1)'
-  ]
-  heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+ ]
+ heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
 }
 
 google.maps.event.addDomListener(window, 'load', initMap);
