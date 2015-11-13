157117f3a974fe3cb0f4ea51621243866eacb62a
Olaf Alexander <o_a1@txstate.edu>
2015-11-13 15:37:53 -0600

    Revert "Revert "Add indicio_theysay app""
    This reverts commit 1769b477011d93714bd2df7e711a0c5cca4b86ae.

--- sentiments/models.py
+++ sentiments/models.py
@@ -1,9 +1,29 @@
 from django.db import models
 
+class Tag(models.Model):
+    tagword = models.CharField(max_length=50, null=True, blank=True)
+
+    def __unicode__(self):
+        return self.tagword
+
+    class Meta:
+        ordering = ('tagword',)
+
 class Post(models.Model):
+    POSITIVE = 'POS'
+    NEGATIVE = 'NEG'
+    SENTM_CHOICES = (
+            (POSITIVE, 'Positive'),
+            (NEGATIVE, 'Negative'),
+    )
+
     timestamp = models.CharField(max_length=50)
     poster = models.CharField(max_length=100)
     statement = models.TextField()
+    tags = models.ManyToManyField(Tag)
+    sentiment = models.CharField(max_length=3, choices=SENTM_CHOICES, null=True, blank=True)
+    value = models.DecimalField(max_digits=19, decimal_places=10, null=True, blank=True)
+
 
     def __unicode__(self):
         return self.statement
