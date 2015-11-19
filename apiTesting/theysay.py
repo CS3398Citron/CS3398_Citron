91d1d926ad535179507d0cd86634f058aeab857a
Bordovsky <bab178@txstate.edu>
2015-11-19 15:00:38 -0600

    Remove clutter files and add delay to Sentiment Analysis

--- apiTesting/theysay.py
+++
@@ -1,8 +0,0 @@
-__import__(affectr)
-
-affectr.set_details("Splitusa@gmail.com", "eeZahyahqu8F")
-
-affectr.client.classify_intent(
-    "We are planning to implement a real-time data service. " +
-    "What are the advantages/disadvantages of using a Foobar-compliant database over Hype.js?"
-)[0].intentType
